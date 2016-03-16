747e20f6d29544cea38237b6e2090a32001e9f8c
iivaniv <ivanyv.ivan@yandex.ua>
2016-03-16 15:16:28 +0200

    update file directories

--- react-pet-prj/src/main.js
+++ react-pet-prj/src/main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppComponent from './AppComponent';
+import AppComponent from './com.edu.component/AppComponent';
 import InfiniteList from './infinite-list/InfiniteList';
 
-ReactDOM.render(<InfiniteList />, document.getElementById('root'));
+ReactDOM.render(<AppComponent />, document.getElementById('root'));
